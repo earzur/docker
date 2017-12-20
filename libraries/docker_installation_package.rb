43178b8844436f01bffae5d9e852a7e7c14ccef9
Erwan Arzur <erwan@arzur.net>
2017-12-20 14:18:28 +0100

    add option to lock the docker package and prevent upgrades when packages are upgraded on the system

--- libraries/docker_installation_package.rb
+++ libraries/docker_installation_package.rb
@@ -12,19 +12,34 @@ module DockerCookbook
     property :package_name, String, default: lazy { default_package_name }, desired_state: false
     property :package_version, String, default: lazy { version_string(version) }, desired_state: false
     property :version, String, default: lazy { default_docker_version }, desired_state: false
+    property :lock_version, kind_of: TrueClass, default: false, desired_state: false
     property :package_options, String, desired_state: false
 
     # Actions
     action :create do
-      package new_resource.package_name do
-        version new_resource.package_version
-        options new_resource.package_options
+      package package_name do
+        version package_version
+        options package_options
         action :install
+      end
+      if new_resource.lock_version
+        case node['platform_family']
+        when 'debian'
+          apt_package package_name do
+            version package_version
+            action :lock
+          end
+        when 'rhel'
+          yum_package package_name do
+            version package_version
+            action :lock
+          end
+        end
       end
     end
 
     action :delete do
-      package new_resource.package_name do
+      package package_name do
         action :remove
       end
     end
